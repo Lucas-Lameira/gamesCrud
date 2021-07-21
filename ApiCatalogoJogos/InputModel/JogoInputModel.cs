0a83a0db7a0fce201ab5080d2e3b67771ae67551
Lucas-Lameira <lucasslameira@gmail.com>
2021-07-21 18:28:35 -0300

    v1 controller inputmodel services and view model

---
+++ ApiCatalogoJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nome inválido")]
+        public string Nome { get; set;}
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Nome da produtora inválido")]
+        public string Produtora { get; set; }
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Preco inválido")]
+        public double Preco { get; set; }
+    }
+}
